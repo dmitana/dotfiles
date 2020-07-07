7b56f8d8cf19408e7b9c6e51a9a8acf98a4b2e6a
Denis Mitana <denis.mitana1@gmail.com>
2020-07-07 16:37:28 +0200

    Change AwesomeWM's hotkeys popup colors

--- .config/awesome/theme/theme.lua
+++ .config/awesome/theme/theme.lua
@@ -47,6 +47,8 @@ theme.border_marked       = "#91231c"
 -- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
 -- Example:
 --theme.taglist_bg_focus = "#ff0000"
+theme.hotkeys_bg = "#222222"
+theme.hotkeys_modifiers_fg = "#bfbfbf"
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
