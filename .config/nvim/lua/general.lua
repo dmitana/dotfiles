0598756b1185b4b41c5fa783c90c2b67532aef6c
Denis Mitana <denis.mitana1@gmail.com>
2021-05-25 19:46:16 +0200

    Add nvim lua general settings
    Set cursorline, hybrid line numbers and colorscheme.

---
+++ .config/nvim/lua/general.lua
@@ -0,0 +1,9 @@
+-- Show cursor line
+vim.wo.cursorline = true
+
+-- Hybrid (number + relative) line numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Enable the colorscheme
+vim.cmd('colorscheme tokyonight')
