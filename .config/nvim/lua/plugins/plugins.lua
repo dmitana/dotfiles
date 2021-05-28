d0b791b93d34707f7e0752c116f11dadd6fa96d2
Denis Mitana <denis.mitana1@gmail.com>
2021-05-28 17:26:58 +0200

    Add comments to the nvim lua plugins

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,20 +1,28 @@
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim' -- A use-package inspired plugin manager for Neovim
 
   -- LSP
-  use 'neovim/nvim-lspconfig'
+  use 'neovim/nvim-lspconfig' -- Quickstart configurations for the Nvim LSP client
 
   -- Autocompletion
-  use 'hrsh7th/nvim-compe'
+  use 'hrsh7th/nvim-compe' -- Auto completion plugin for nvim that written in Lua
 
   -- Syntax highlighting
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { -- Nvim Treesitter configurations and abstraction layer
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- General coding support
+  use { -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
+    'folke/trouble.nvim',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
 
   -- Themes and styling
-  use { 'npxbr/gruvbox.nvim', requires = 'rktjmp/lush.nvim' }
-  use 'folke/tokyonight.nvim'
-  use 'joshdick/onedark.vim'
+  use { 'npxbr/gruvbox.nvim', requires = 'rktjmp/lush.nvim' } -- Lua port of the most famous vim colorscheme
+  use 'folke/tokyonight.nvim' -- A clean, dark Neovim theme written in Lua
+  use 'joshdick/onedark.vim' -- A dark Vim/Neovim color scheme inspired by Atom's One Dark syntax theme
 
-  use { 'folke/trouble.nvim', requires = 'kyazdani42/nvim-web-devicons' }
 end)
