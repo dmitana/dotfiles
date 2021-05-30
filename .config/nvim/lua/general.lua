ac018226698e5aea090f7deb5d7219bea5209165
Denis Mitana <denis.mitana1@gmail.com>
2021-05-30 10:20:18 +0200

    Update nvim lua general settings

--- .config/nvim/lua/general.lua
+++ .config/nvim/lua/general.lua
@@ -1,9 +1,7 @@
--- Show cursor line
-vim.wo.cursorline = true
-
--- Hybrid (number + relative) line numbers
-vim.wo.number = true
-vim.wo.relativenumber = true
+-- Settings
+--
+-- Load filetype plugin and indent files
+vim.cmd 'filetype plugin indent on'
 
 -- Enable syntax highlighting
 vim.cmd 'syntax on'
@@ -13,3 +11,45 @@ vim.cmd 'set termguicolors'
 
 -- Enable the colorscheme
 vim.cmd 'colorscheme onedark'
+
+-- Access system clipboard in VIM
+vim.cmd 'set clipboard=unnamed'
+
+-- Turn off search highlighting
+vim.cmd 'set nohlsearch'
+
+-- Show tab line even if only one file is open
+vim.cmd 'set showtabline=2'
+
+-- Show cursor line
+vim.wo.cursorline = true
+
+-- Hybrid (number + relative) line numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Enable to leave a buffer with unsaved changes
+vim.cmd 'set hidden'
+
+-- Always show sign columns
+vim.cmd 'set signcolumn=yes'
+
+-- Keybindings
+--
+-- Navigation in split windows
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w><C-j>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><C-k>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><C-l>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><C-h>', { noremap = true, silent = true })
+
+-- Tab navigation
+vim.api.nvim_set_keymap('n', '<M-l>', 'gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-h>', 'gT', { noremap = true, silent = true })
+
+-- Map yanking to PRIMARY and CLIPBOARD registers PRIMARY (*) mnemonic: Star is
+-- Select (for copy-on-select) CLIPBOARD (+) mnemonic: CTRL PLUS C (for the
+-- common keybind)
+vim.api.nvim_set_keymap('', '<Leader>y', '"*y', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('', '<Leader>p', '"*p', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('', '<Leader>Y', '"+Y', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('', '<Leader>P', '"+P', { noremap = true, silent = false })
