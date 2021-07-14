38a7bc19e6b554d877eadd8d0fe60cc5c9722e83
Denis Mitana <denis.mitana1@gmail.com>
2021-07-14 20:18:50 +0200

    Add general settings from old init.vim
    Add autoopen of a quickfix list, navigation in a quickfix list, search
    for visually selected text, and general file configuration.

--- .config/nvim/lua/general.lua
+++ .config/nvim/lua/general.lua
@@ -18,6 +18,9 @@ vim.cmd 'set clipboard=unnamed'
 -- Turn off search highlighting
 vim.cmd 'set nohlsearch'
 
+-- Ignore case in search patterns (case insensitive search)
+vim.opt.ignorecase = true
+
 -- Show tab line even if only one file is open
 vim.cmd 'set showtabline=2'
 
@@ -39,6 +42,22 @@ vim.cmd 'set signcolumn=yes'
 -- nvim-lightbuld.
 vim.opt.updatetime = 300
 
+-- Autoopen of quickfix list (etc. after vimgrep)
+vim.cmd [[
+  augroup autoquickfix
+      autocmd!
+      autocmd QuickFixCmdPost [^l]* cwindow
+      autocmd QuickFixCmdPost    l* lwindow
+  augroup END
+]]
+
+-- General file configuration
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.encoding = 'utf-8'
+
 -- Keybindings
 --
 -- Navigation in split windows
@@ -51,6 +70,10 @@ vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><C-h>', { noremap = true, silent = t
 vim.api.nvim_set_keymap('n', '<M-l>', 'gt', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<M-h>', 'gT', { noremap = true, silent = true })
 
+-- Quickfix list navigation
+vim.api.nvim_set_keymap('n', ']q', ':cnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[q', ':cprevious<CR>', { noremap = true, silent = true })
+
 -- Map yanking to PRIMARY and CLIPBOARD registers PRIMARY (*) mnemonic: Star is
 -- Select (for copy-on-select) CLIPBOARD (+) mnemonic: CTRL PLUS C (for the
 -- common keybind)
@@ -62,3 +85,6 @@ vim.api.nvim_set_keymap('', '<Leader>P', '"+p', { noremap = true, silent = false
 -- Terminal
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = false })
 vim.api.nvim_set_keymap('t', '<C-[>', '<C-\\><C-n>', { noremap = true, silent = false })
+
+-- Search for visually selected text using //
+vim.api.nvim_set_keymap('v', '//', 'y/\\V<C-R>=escape(@","/\")<CR><CR>', { noremap = true, silent = false })
