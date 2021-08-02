2746d8f2c4ffe2a88c44e5b19fd639cd252764ab
Denis Mitana <denis.mitana1@gmail.com>
2021-08-02 09:15:09 +0200

    Add python fstring support to pears

--- .config/nvim/lua/plugins/pears.lua
+++ .config/nvim/lua/plugins/pears.lua
@@ -1,5 +1,19 @@
+local R = require'pears.rule'
+
 require'pears'.setup(function(conf)
   conf.preset 'tag_matching'
+
+  -- Python f-string pairs
+  conf.pair("f'", {
+    close = "'",
+    filetypes = { 'python' },
+    should_expand = R.not_(R.child_of_node'string')
+  })
+  conf.pair('f"', {
+    close = '"',
+    filetypes = { 'python' },
+    should_expand = R.not_(R.child_of_node'string')
+  })
 
   -- Completion integration
   conf.on_enter(function(pears_handle)
