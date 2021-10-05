88b96985804a40d880bb907d29a508bb1ef4512f
Denis Mitana <denis.mitana1@gmail.com>
2021-10-05 13:21:13 +0200

    Update nvim-tree configuration to the new version

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,47 +1,22 @@
--- Keybindings
-vim.api.nvim_set_keymap(
-  'n',
-  '<C-n>',
-  ':NvimTreeToggle<CR>',
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  'n',
-  '<leader>r',
-  ':NvimTreeRefresh<CR>',
-  { noremap = true, silent = true }
-)
-
--- Settings
-vim.g.nvim_tree_side = 'left'
-vim.g.nvim_tree_width = 40
+-- Non setup settings (should be set BEFORE running the setup function)
 vim.g.nvim_tree_ignore = { '.git' }
 vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_open = 0 -- Opens the tree when typing `vim $DIR` or `vim`
-vim.g.nvim_tree_auto_close = 1 -- Closes the tree when it's the last window
-vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' } -- Don't auto open tree on specific filetypes
 vim.g.nvim_tree_quit_on_open = 0 -- Closes the tree when you open a file
-vim.g.nvim_tree_follow = 1 -- This option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_indent_markers = 1 -- Indent markers when folders are open
 vim.g.nvim_tree_hide_dotfiles = 1 -- This option hides files and folders starting with a dot `.`
 vim.g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_highlight_opened_files = 1 -- Will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
-vim.g.nvim_tree_tab_open = 0 -- Will open the tree when entering a new tab and the tree was previously open
-vim.g.nvim_tree_width_allow_resize  = 1 -- Will resize the tree when opening a file
-vim.g.nvim_tree_disable_netrw = 0 -- Disables netrw
-vim.g.nvim_tree_hijack_netrw = 0 -- Prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
 vim.g.nvim_tree_add_trailing = 1 -- Append a trailing slash to folder names
 vim.g.nvim_tree_group_empty = 1 -- Compact folders that only contain a single folder into one node in the file tree
-vim.g.nvim_tree_lsp_diagnostics = 1 -- Will show lsp diagnostics in the signcolumn. See :help nvim_tree_lsp_diagnostics
 vim.g.nvim_tree_disable_window_picker = 1 -- Will disable the window picker.
--- Dictionary of buffer option names mapped to a list of option values that
--- indicates to the window picker that the buffer's window should not be
--- selectable.
 vim.g.nvim_tree_window_picker_exclude = {
   filetype = { 'packer', 'qf' },
   buftype = { 'terminal' }
 }
+-- Dictionary of buffer option names mapped to a list of option values that
+-- indicates to the window picker that the buffer's window should not be
+-- selectable.
 -- List of filenames that gets highlighted with NvimTreeSpecialFile
 vim.g.nvim_tree_special_files = {
   ["Cargo.toml"] = true,
@@ -85,4 +60,80 @@ vim.g.nvim_tree_icons = {
   }
 }
 
--- A list of groups can be found at `:help nvim_tree_highlight`
+require'nvim-tree'.setup {
+  -- disables netrw completely
+  disable_netrw = false,
+  -- hijack netrw window on startup
+  hijack_netrw = true,
+  -- open the tree when running this setup function
+  open_on_setup = false,
+  -- will not open on setup if the filetype is in this list
+  ignore_ft_on_setup = { 'startify', 'dashboard' },
+  -- closes neovim automatically when the tree is the last **WINDOW** in the view
+  auto_close = true,
+  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+  open_on_tab = false,
+  -- hijacks new directory buffers when they are opened.
+  update_to_buf_dir   = {
+    -- enable the feature
+    enable = true,
+    -- allow to open the tree if it was previously closed
+    auto_open = true,
+  },
+  -- hijack the cursor in the tree to put it at the start of the filename
+  hijack_cursor = false,
+  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+  update_cwd = false,
+  -- show lsp diagnostics in the signcolumn
+  lsp_diagnostics = true,
+  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+  update_focused_file = {
+    -- enables the feature
+    enable = true,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd = false,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {}
+  },
+  -- configuration options for the system open command (`s` in the tree by default)
+  system_open = {
+    -- the command to run this, leaving nil should work in most cases
+    cmd = nil,
+    -- the command arguments as a list
+    args = {}
+  },
+  view = {
+    -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
+    width = 40,
+    -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+    height = 30,
+    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+    side = 'left',
+    -- if true the tree will resize itself after opening a file
+    auto_resize = false,
+    mappings = {
+      -- custom only false will merge the list with the default mappings
+      -- if true, it will only use your list to set the mappings
+      custom_only = false,
+      -- list of mappings to set on the tree manually
+      list = {}
+    }
+  },
+  tab_open = false,
+}
+
+-- Keybindings
+vim.api.nvim_set_keymap(
+  'n',
+  '<C-n>',
+  ':NvimTreeToggle<CR>',
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>r',
+  ':NvimTreeRefresh<CR>',
+  { noremap = true, silent = true }
+)
