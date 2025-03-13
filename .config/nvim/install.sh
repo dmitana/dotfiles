ce26d408d7b87faf8281107df855f15aed8db94e
Denis Mitana <denis.mitana1@gmail.com>
2025-03-13 21:26:20 +0100

    Update nvim installation

--- .config/nvim/install.sh
+++ .config/nvim/install.sh
@@ -5,42 +5,37 @@ yay -S --noconfirm wget curl git ripgrep g++
 yay -S --noconfirm neovim
 
 # Install the Packer plugin manager
-git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+yay -S --noconfirm packer
 
 # Install python3 if it doesn't exist
 if ! command -v python3 &> /dev/null
 then
-    yay -S --noconfirm python3 python3-distutils
+    yay -S --noconfirm python3 python-setuptools
 fi
 
 # Install pip3 if it doesn't exist
 if ! command -v pip3 &> /dev/null
 then
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3 get-pip.py
-    rm get-pip.py
+    yay -S --noconfirm python-pip
 fi
 
-# Upgrade pip
-pip3 install pip --upgrade
-
 # Install the NeoVim Python module
-pip3 install --user neovim pynvim
+yay -S python-pynvim
 
 # Install nodejs if doesn't exist
 if ! command -v node &> /dev/null
 then
-    yay -S --noconfirm nodejs-lts-erbium npm
+    yay -S --noconfirm nodejs-lts-iron npm
 fi
 
 # LSP installation
 #
 # Python (pyright + pylsp)
 sudo npm i -g pyright
-pip3 install --user -r ~/.config/nvim/requirements-dev.txt
+yay -S $(cat ~/.config/nvim/requirements-dev-yay.txt)
 
 # Go (gopls)
-GO111MODULE=on go get golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/gopls@latest
 
 # Eslint (JS, JS), JSON, HTML and CSS (SCSS, LESS)
 sudo npm i -g vscode-langservers-extracted
